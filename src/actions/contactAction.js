a7efefbe124ddecbdae8f3294e8d76ed62c8eb35
Gábor Horváth <dev.gaborhorvath@gmail.com>
2018-10-18 23:52:00 +0200

    Get user by id from server

--- src/actions/contactAction.js
+++ src/actions/contactAction.js
@@ -1,4 +1,10 @@
-import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from "./types";
+import {
+  GET_CONTACTS,
+  DELETE_CONTACT,
+  ADD_CONTACT,
+  GET_CONTACT,
+  UPDATE_CONTACT
+} from "./types";
 import axios from "axios";
 
 export const getContacts = () => async dispatch => {
@@ -6,6 +12,17 @@ export const getContacts = () => async dispatch => {
 
   dispatch({
     type: GET_CONTACTS,
+    payload: response.data
+  });
+};
+
+export const getContact = id => async dispatch => {
+  const response = await axios.get(
+    `http://jsonplaceholder.typicode.com/users/${id}`
+  );
+
+  dispatch({
+    type: GET_CONTACT,
     payload: response.data
   });
 };
